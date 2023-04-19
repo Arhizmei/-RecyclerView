421fcea49bbcacbeb92a1ef04227b7523494ef98
arhizmei <arhizmei@gmail.com>
2023-04-19 14:19:08 +0300

    work3

--- app/src/main/java/com/zmei/recyclerview/MainActivity.kt
+++ app/src/main/java/com/zmei/recyclerview/MainActivity.kt
@@ -2,10 +2,13 @@ package com.zmei.recyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.zmei.recyclerview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var bind : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        bind = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bind.root)
     }
 }
