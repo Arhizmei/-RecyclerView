854fd42ca1c649bae9f304af2037fdd6b1e48322
arhizmei <arhizmei@gmail.com>
2023-04-22 21:00:10 +0300

    work6

--- app/src/main/java/com/zmei/recyclerview/MainActivity.kt
+++ app/src/main/java/com/zmei/recyclerview/MainActivity.kt
@@ -2,13 +2,24 @@ package com.zmei.recyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
 import com.zmei.recyclerview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var bind : ActivityMainBinding
+    private val adapter = AdapterClass()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         bind = ActivityMainBinding.inflate(layoutInflater)
         setContentView(bind.root)
+        init()
+    }
+    private fun init(){
+        bind.apply {
+            recyclerID.layoutManager = GridLayoutManager(this@MainActivity, 3)
+            recyclerID.adapter = adapter
+
+        }
+
     }
 }
