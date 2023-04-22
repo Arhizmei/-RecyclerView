8eb59e77fa20b30d63ebde27b826e832d80aafe6
arhizmei <arhizmei@gmail.com>
2023-04-22 21:37:21 +0300

    work7

--- app/src/main/java/com/zmei/recyclerview/MainActivity.kt
+++ app/src/main/java/com/zmei/recyclerview/MainActivity.kt
@@ -8,6 +8,13 @@ import com.zmei.recyclerview.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     lateinit var bind : ActivityMainBinding
     private val adapter = AdapterClass()
+    private val imageIdList = listOf(
+                R.drawable.plant1,
+                R.drawable.plant2,
+                R.drawable.plant3,
+                R.drawable.plant4,
+                R.drawable.plant5)
+    private var index = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         bind = ActivityMainBinding.inflate(layoutInflater)
@@ -18,6 +25,13 @@ class MainActivity : AppCompatActivity() {
         bind.apply {
             recyclerID.layoutManager = GridLayoutManager(this@MainActivity, 3)
             recyclerID.adapter = adapter
+            button.setOnClickListener {
+                if (index>4) index =0
+                val plant = PlantDataClass(imageIdList[index], "Plant $index")
+                adapter.addPlant(plant)
+                index++
+
+            }
 
         }
 
